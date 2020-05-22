3fdfd2d7748290269a118aa133523bc00606c114
numbers0580 <koreankid18@yahoo.com>
2020-05-22 10:28:43 -0600

    Completed arrays-callbacks

--- challenges/arrays-callbacks.js
+++ challenges/arrays-callbacks.js
@@ -81,6 +81,10 @@ console.log(populationTotal);
   * The consume function should return the invocation of cb, passing a and b into cb as arguments
 */
 
+function consume(a, b, cb) {
+  return cb(a, b);
+}
+
 
 /* Step 2: Create several functions to callback with consume();
   * Create a function named add that returns the sum of two numbers
@@ -88,11 +92,23 @@ console.log(populationTotal);
   * Create a function named greeting that accepts a first and last name and returns "Hello first-name last-name, nice to meet you!"
 */
 
+function add(aVal, bVal) {
+  return aVal + bVal;
+}
 
+function multiply(aVal, bVal) {
+  return aVal * bVal;
+}
+
+function greeting(fName, lName) {
+  return "Hello " + fName + " " + lName + ", nice to meet you!";
+}
+
+console.log("Arrays-Callbacks: Callbacks steps 1-3 below");
 /* Step 3: Check your work by un-commenting the following calls to consume(): */
-// console.log(consume(2, 2, add)); // 4
-// console.log(consume(10, 16, multiply)); // 160
-// console.log(consume("Mary", "Poppins", greeting)); // Hello Mary Poppins, nice to meet you!
+console.log(consume(2, 2, add)); // 4
+console.log(consume(10, 16, multiply)); // 160
+console.log(consume("Mary", "Poppins", greeting)); // Hello Mary Poppins, nice to meet you!
 
 
 
